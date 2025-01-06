6adeb3c6a91772d203877781fd82557db17fd6bc
Aliaksei Stryi <aliaksei.stryi@postnord.com>
2025-01-06 23:15:28 +0100

    Fixed the week number calculation

--- desklet.js
+++ desklet.js
@@ -57,7 +57,7 @@ WeekNowDesklet.prototype = {
   },
 
   getCurrentWeekNumber () {
-    const weekNum = this.getDateWeek()
+    const weekNum = this.getWeekNumber()
 
     this.layoutItems = [
       createLabel(''+weekNum, 'week-now-week-number')
@@ -89,20 +89,23 @@ WeekNowDesklet.prototype = {
   on_desklet_removed: function() {
   },
 
-  getDateWeek(date) {
-    const currentDate = (typeof date === 'object') ? date : new Date();
-    const januaryFirst = new Date(currentDate.getFullYear(), 0, 1);
-    const daysToNextMonday = (januaryFirst.getDay() === 1)
-      ? 0
-      : (7 - januaryFirst.getDay()) % 7;
+  getWeekNumber(date) {
+    const curDate = (typeof date === 'object') ? date : new Date();
+    // Copy date so don't modify original
 
-    const nextMonday = new Date(currentDate.getFullYear(), 0, januaryFirst.getDate() + daysToNextMonday);
+    const d = new Date(Date.UTC(curDate.getFullYear(), curDate.getMonth(), curDate.getDate()));
+    // Set to nearest Thursday: current date + 4 - current day number
+    // Make Sunday's day number 7
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
 
-    return (currentDate < nextMonday)
-      ? 52
-      : currentDate > nextMonday
-        ? Math.ceil((currentDate - nextMonday) / (24 * 3600 * 1000) / 7)
-        : 1;
+    // Get first day of year
+    const yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+
+    // Calculate full weeks to nearest Thursday
+    const weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1) / 7);
+
+    // Return array of year and week number
+    return weekNo
   }
 };
 
